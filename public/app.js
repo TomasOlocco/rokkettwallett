b7da8d7955e577a4d09613455069090147825b57
Tomás Olocco <tomasolocco04@gmail.com>
2024-07-22 20:57:13 -0300

    22/07 generar ID

--- public/app.js
+++ public/app.js
@@ -2,7 +2,27 @@ const app = Vue.createApp({
     data() {
         return {
             logo: 'ROKKETT',
-            imagenlogo: 'https://png.pngtree.com/png-clipart/20230102/original/pngtree-cartoon-illustration-red-rocket-png-image_8856222.png'
+            imagenlogo: 'https://png.pngtree.com/png-clipart/20230102/original/pngtree-cartoon-illustration-red-rocket-png-image_8856222.png',
+            idAlf: "",
+            nombre: '',
+            apellido: '',
+            error: " "
         };
+    },
+  
+    methods: {
+      submitForm() {
+        error = "Ingrese su nombre";
+      }
+    },
+
+    computed: {
+      generarId(){
+        if ({
+          nombre: !String.Empty,
+          apellido: !String.Empty
+        })
+        this.idAlf = this.nombre + this.apellido + Math.random().toString(36);
+      }
     }
 });
